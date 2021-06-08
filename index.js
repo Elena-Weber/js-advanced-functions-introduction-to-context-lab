7a1c7ab908261d26009390781d9790f5d85ef8f3
Elena-Weber <crochetmiracles@gmail.com>
2021-06-07 20:22:44 -0400

    commit tree 9beab2cc73d68f71f83b427435ea4d18abf9b7d1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code here
+function createEmployeeRecord(arr) {
+    let timeInEvents = [];
+    let timeOutEvents = [];
+    let firstName = arr[0];
+    let familyName = arr[1];
+    let title = arr[2];
+    let payPerHour = arr[3]
+}
